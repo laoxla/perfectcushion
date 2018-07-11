ff5b6098423037e711fb2e7773041630847a96af
laoxla <oasiyanbola@gmail.com>
2018-07-11 14:22:33 -0400

    Added subract and remove functionality to cart

--- perfectcushion/settings.py
+++ perfectcushion/settings.py
@@ -57,7 +57,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'shop.context_processors.menu_links'
+                'shop.context_processors.menu_links',
+                'cart.context_processors.counter',
             ],
         },
     },
